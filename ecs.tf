76a72486b76bd46a68ff1123eeb1249a76e2a20f
Agustin Larrarte <>
2020-08-20 23:52:34 -0300

    adding structure

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,19 @@
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = var.cluster_name
 }
+
+
+resource "aws_ecs_task_definition" "test_task" {
+  family                = "service"
+  container_definitions = file("task-definitions/webserver.json")
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
